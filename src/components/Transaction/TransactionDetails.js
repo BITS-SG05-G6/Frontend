1acd9b137e6a57b82173b7ca09e56de92390488b
xian-94 <s3957034@rmit.edu.vn>
2023-12-05 11:20:35 +0700

    add header

--- src/components/Transaction/TransactionDetails.js
+++ src/components/Transaction/TransactionDetails.js
@@ -1,35 +1,35 @@
 import Button from "../common/Button";
 
-function TransactionDetails({ title, amount }) {
+function TransactionDetails({ title, amount, category, wallet, note, type, date }) {
     return (
         <div className="w-96 h-[660px] bg-white rounded-[10px] border border-gray-300">
             <div className="px-2 py-3 flex flex-col gap-2">
-                <div className="text-gray-800 text-xl font-semibold text-center">Iphone 13 Pro Max</div>
-                <div className="text-red-400 text-lg font-bold text-center">$420.84</div>
+                <div className="text-gray-800 text-xl font-semibold text-center">{title}</div>
+                <div className="text-red-400 text-lg font-bold text-center">{amount}VND</div>
             </div>
             <div className="px-5 py-3">
                 <table className='table'>
                     <tr>
                         <th className="text-gray-400">Type</th>
-                        <td className="font-semibold text-end">Expense</td>
+                        <td className="font-semibold text-end">{type}</td>
                     </tr>
                     <tr>
                         <th className='text-gray-400'>Category</th>
-                        <td className="text-end font-semibold"><span className="bg-[#FCDDEC] rounded-[10px] px-4 py-1">Food & Drink</span></td>
+                        <td className="text-end font-semibold"><span className="bg-[#FCDDEC] rounded-[10px] px-4 py-1">{category}</span></td>
                     </tr>
                     <tr>
                         <th className='text-gray-400'>Wallet</th>
-                        <td className="text-end font-semibold"><span className="bg-[#A5A6F6] rounded-[10px] px-4 py-1">Credit card</span></td>
+                        <td className="text-end font-semibold"><span className="bg-[#A5A6F6] rounded-[10px] px-4 py-1">{wallet}</span></td>
                     </tr>
                     <tr>
                         <th className='text-gray-400'>Date</th>
-                        <td className="text-end font-semibold"><span className="bg-slate-200 rounded-[10px] px-3 py-1">Date</span></td>
+                        <td className="text-end font-semibold"><span className="bg-slate-200 rounded-[10px] px-3 py-1">{date}</span></td>
                     </tr>
                 </table>
             </div>
             <div className="px-4 py-3 flex flex-col gap-2">
                 <div className="text-gray-400 font-semibold text-sm ms-5">Note</div>
-                <div className="bg-slate-50 border rounded p-2 text-sm mx-4 my-2">Note</div>
+                <textarea className="texarea textarea-md border-slate-400 rounded border text-sm mx-4 my-2 p-2" placeholder={note} disabled></textarea>
             </div>
             <div className="py-3 flex gap-2 justify-center">
                 <Button children='Edit' variant='blueButton' size='lg'/>
--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -3,13 +3,14 @@ import TransactionList from "../components/Transaction/TransactionList";
 import TransactionDetails from "../components/Transaction/TransactionDetails";
 import SideBar from "../components/common/SideBar";
 import Button from "../components/common/Button";
+import Header from "../components/common/Header";
 
 function Transaction() {
     return (
         <div className="flex">
             <SideBar />
             <div className="ms-72">
-                {/* Header here... */}
+                <Header title='Transactions' username='Tom Vo'/>
                 <div className="text-end my-3">
                     <Button children='Create Transaction' />
                 </div>
