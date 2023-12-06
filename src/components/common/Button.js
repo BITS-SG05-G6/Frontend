c3c6733390d58bde664049a727ef541a6b9de246
xian-94 <s3957034@rmit.edu.vn>
2023-12-06 16:50:55 +0700

    fix ui components

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { cva } from "class-variance-authority";
+import { cva } from 'class-variance-authority';
 import { cn } from "../../utils/cn";
 import { Link } from "react-router-dom";
 
@@ -8,42 +8,48 @@ const ButtonVariants = cva(
   {
     variants: {
       variant: {
-        roundOutline:
-          "btn hover:bg-[#EF5DA8] bg-transparent border-[#EF5DA8] hover:text-white text-[#EF5DA8]",
-        cirFill:
-          "btn bg-[#EF5DA8] hover:bg-transparent hover:border-[#EF5DA8] text-white hover:text-[#EF5DA8] rounded-full",
-        cirOutline:
-          "btn hover:bg-[#EF5DA8] bg-transparent border-[#EF5DA8] hover:text-white text-[#EF5DA8] rounded-full",
-        cirTrans:
-          "btn bg-[#FCDDEC] hover:bg-transparent hover:border-[#EF5DA8] text-white hover:text-[#EF5DA8] rounded-full",
+        roundOutline: 'btn hover:bg-[#EF5DA8] bg-transparent border-[#EF5DA8] hover:text-white text-[#EF5DA8]',
+        cirFill: 'btn bg-[#EF5DA8] hover:bg-transparent hover:border-[#EF5DA8] text-white hover:text-[#EF5DA8] rounded-full',
+        cirOutline: 'btn hover:bg-[#EF5DA8] bg-transparent border-[#EF5DA8] hover:text-white text-[#EF5DA8] rounded-full',
+        cirTrans: 'btn bg-[#FCDDEC] hover:bg-transparent hover:border-[#EF5DA8] text-white hover:text-[#EF5DA8] rounded-full',
+        redButton: 'btn bg-red-400 hover:bg-transparent hover:border-red-400 text-white hover:text-red-400 rounded-[10px]',
+        blueButton: 'btn bg-[#7879F1] hover:bg-transparent hover:border-[#7879F1] text-white hover:text-[#7879F1] rounded-[10px]',
+        navButton: 'py-3 ps-4 pe-20 rounded-lg bg-neutral-50 hover:bg-pink-400 border-none shadow-none'
+
       },
       size: {
-        default: "h-10 py-2 px-4 text-sm",
-        sm: "h-9 py-2 rounded-md text-xs",
-        lg: "h-11 px-11 text-sm",
-      },
+        default: 'h-10 py-2 px-4 text-sm',
+        sm: 'h-9 py-2 rounded-md text-xs',
+        lg: 'h-11 px-11 text-sm'
+      }
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: 'default',
+      size: 'default'
     },
   }
-);
+)
 
-const Button = ({ children, variant, size, className, href }) => {
+const Button = ({ children, variant, size, className, href, onMouseEnter, onMouseLeave }) => {
   return (
-  href ? (
-    <Link
-      className={cn(ButtonVariants({ variant, size, className }))}
-      to={href}
-    >
-      {children}
-    </Link>
-  ) : (
-    <button className={cn(ButtonVariants({ variant, size, className }))}>
-      {children}
-    </button>
-  ));
+    href ? (
+      <Link
+        className={cn(ButtonVariants({ variant, size, className }))}
+        to={href}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        {children}
+      </Link>
+    ) : (
+      <button className={cn(ButtonVariants({ variant, size, className }))}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}>
+        {children}
+      </button>
+    ));
 };
+
+
 
 export default Button;
