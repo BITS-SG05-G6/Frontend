862b089f55c3be3e42021fc3103f8b2bd4459e12
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-12-07 22:15:50 +0700

    done signin, singup, landing, transaction page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import Test from './pages/Test';
 import Transaction from './pages/Transaction';
 // import { SignUp } from "./pages/Signup";
 import Login from './pages/Login';
-import { Login2 } from './pages/Login2';
 import Signup from './pages/Signup';
 // import { Login } from "./pages/Login";
 import { SignUp } from "./pages/Signup";
-import Transaction2 from './pages/Transaction2';
 
 function App() {
   return (
@@ -19,12 +17,10 @@ function App() {
        <Route path="/test" element={<Test/>}></Route>
        <Route path="/transaction" element={<Transaction/>}></Route>
        <Route path="/login" element={<Login/>}></Route>
-       <Route path="/login2" element={<Login2/>}></Route>
 
       {/* <Route path="/login" element={<Login />}></Route> */}
       <Route path="/signup" element={<Signup/>}></Route>
 
-       <Route path="/transaction2" element={<Transaction2/>}></Route>
   
     </Routes>
   );
