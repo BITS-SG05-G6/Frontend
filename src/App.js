c7079eaa07a1e2e05c0766649c85d8c55546dd2a
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-12-07 19:09:32 +0700

    update Transaction

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Test from './pages/Test';
 import Transaction from './pages/Transaction';
 import { Login } from "./pages/Login";
 import { SignUp } from "./pages/Signup";
+import Transaction2 from './pages/Transaction2';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
        <Route path="/" element={<LandingPage/>}></Route>
        <Route path="/test" element={<Test/>}></Route>
        <Route path="/transaction" element={<Transaction/>}></Route>
+       <Route path="/transaction2" element={<Transaction2/>}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/signup" element={<SignUp />}></Route>
     </Routes>
