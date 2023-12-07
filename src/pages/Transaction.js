bdbed131636cbaea7bb481c2ebc3bcbe49598557
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-12-07 20:34:56 +0700

    modify sidebar and transaction page

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -25,7 +25,7 @@ const Transaction = () => {
             <TransactionForm />
           </div>
           <div className="flex justify-between px-10">
-            <div className="flex flex-col flex-1 gap-10">
+            <div className="flex flex-col flex-1 gap-10 pr-5">
               <TransactionCalendar
                 selectedDate={selectedDate}
                 onDateChange={handleDateChange}
