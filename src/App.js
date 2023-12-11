7f41071e2612264a5cc72a365374b4a32e0de2e6
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-12-11 21:53:22 +0700

    remove duplicate file

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import { Route, Routes } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import Test from './pages/Test';
 import Transaction from './pages/Transaction';
-// import { SignUp } from "./pages/Signup";
 import Login from './pages/Login';
-import { Login2 } from './pages/Login2';
 import Signup from './pages/Signup';
-import { Login } from "./pages/Login";
-import { SignUp } from "./pages/Signup";
-import Transaction2 from './pages/Transaction2';
+
 
 function App() {
   return (
@@ -19,12 +15,11 @@ function App() {
        <Route path="/test" element={<Test/>}></Route>
        <Route path="/transaction" element={<Transaction/>}></Route>
        <Route path="/login" element={<Login/>}></Route>
-       <Route path="/login2" element={<Login2/>}></Route>
+
 
       {/* <Route path="/login" element={<Login />}></Route> */}
       <Route path="/signup" element={<Signup/>}></Route>
 
-       <Route path="/transaction2" element={<Transaction2/>}></Route>
   
     </Routes>
   );
