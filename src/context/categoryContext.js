4c5369ec51dc0f697c37d52b300acddf7ffd546e
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-12-14 22:12:39 +0700

    fix bug context

--- src/context/categoryContext.js
+++ src/context/categoryContext.js
@@ -1,43 +1,56 @@
-import { createContext, useEffect, useState } from "react"
-import * as axiosInstance from "../services/category"
+import { createContext, useEffect, useState } from "react";
+import * as axiosInstance from "../services/category";
 
-export const CategoryContext = createContext(null)
+export const CategoryContext = createContext(null);
 
-const CategoryProvider = ({children}) => {
+const CategoryProvider = ({ children }) => {
   const [newCategory, setNewCategory] = useState(false);
 
   const handleAddCategory = () => {
-    setNewCategory(newCategory === "true" ? "false" : "true")
-  }
+    setNewCategory(newCategory === "true" ? "false" : "true");
+  };
 
   const [type, setType] = useState("Expense");
-  const [categories, setCategories] = useState([{
-    id: null,
-    name: null,
-    icon: null,
-    color: null,
-  }])
+  const [categories, setCategories] = useState([
+    {
+      id: null,
+      name: null,
+      icon: null,
+      color: null,
+    },
+  ]);
 
   useEffect(() => {
     async function fetchData() {
-      await axiosInstance.getCategory(type)
-    .then((res) => {
-      setCategories(res);
-    })
+      try {
+        await axiosInstance.getCategory(type).then((res) => {
+          setCategories(res);
+        });
+      } catch (err) {
+        setCategories([{
+          id: "",
+          name: "",
+          icon: "",
+          color: ""
+        }]);
+      }
     }
 
     fetchData();
-
-  }, [type, newCategory])
+  }, [type, newCategory]);
 
   const categoryList = {
     handleAddCategory,
     type,
     setType,
     categories,
-  }
+  };
 
-  return (<CategoryContext.Provider value={categoryList}>{children}</CategoryContext.Provider>)
-}
+  return (
+    <CategoryContext.Provider value={categoryList}>
+      {children}
+    </CategoryContext.Provider>
+  );
+};
 
 export default CategoryProvider;
