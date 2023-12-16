1a74d60f784b7279fc331c6fffecb32c6d0f6ddb
plqanhh <105305089+plqanhh@users.noreply.github.com>
2023-12-17 03:07:58 +0700

    Finalize Wallet Page and Card
    Finalize

--- src/components/Wallet/WalletCard.js
+++ src/components/Wallet/WalletCard.js
@@ -1,86 +1,45 @@
-import React from 'react';
-import Text from '../common/Text';
-import Icon from '../common/Icon';
-import Box from '../common/Box'
-import { WalletIcon } from '../svgs/sidebarIcons';
+import React from "react";
+import Text from "../common/Text";
+import Icon from "../common/Icon";
+import Box from "../common/Box";
+import { WalletIcon } from "../svgs/sidebarIcons";
 import { Progress } from "@material-tailwind/react";
 
 const WalletCard = ({ title, amount, used, color }) => {
- return (
-<<<<<<< Updated upstream
-    <Box className="flex justify-center items-center flex-col gap-10 h-60 w-30" color={"gray"} >
-        <>
-            <Text  weight="semibold" variant="text-base" className="pt-10" >
-                {title}
-            </Text>
-                <div className="rounded-xl p-4" style={{ backgroundColor: `${color}30` }}>
-                <Icon svg={<WalletIcon/>} className="flex item-center" isHovered={"isHovered"} fillColor={color}/>
-                </div>
-            <div>
-                <Text weight={"bold"} variant={"text-base"}>{used}$ /</Text>
-                <Text weight={"bold"} variant={"text-base"}>{amount}$ </Text>
-            </div>
-            <div>
-                <Progress value={(used/amount)*100} color='black'/>
-            </div>
-        </>
-        
-    </Box>
-        
-=======
-    <div className='w-full max-w-sm p-4 bg-gray-100 border border-gray-200 rounded-lg shadow sm:p-8 space-y-10 hover:'>
-        <div>
-            <Text className="text-center ml-10 mr-10 " weight="semibold" variant="text-base" >{title}</Text>
-        </div>
-        <div>
-            <Icon svg={<WalletIcon/>} className="flex item-center" isHovered={"isHovered"} hoverColor={"#EF5DA8"} fillColor={"#FFFFFF"}/>
-        </div>
-        <div>
-            <Text weight={"bold"} variant={"text-base"}>{used}$ /</Text>
-            <Text weight={"bold"} variant={"text-base"}>{amount}$ </Text>
-        </div>
-        <div>
-            <Progress value={(used/amount)*100}/>
-        </div>
+  return (
+    <div
+      className="flex justify-center items-center flex-col gap-11 border rounded-lg shadow sm:p-8"
+      style={{ backgroundColor: `${color}30` }}
+    >
+      <div>
+        <Text className=" " weight="semibold" variant="text-md">
+          {title}
+        </Text>
+      </div>
+      <div className="p-4 rounded-xl" style={{ backgroundColor: `${color}30` }}>
+        <Icon
+          svg={<WalletIcon />}
+          isHovered={"isHovered"}
+          hoverColor={{ color }}
+          fillColor={"#FFFFFF"}
+        />
+      </div>
+      <div>
+        <Text weight={"bold"} variant={"text-base"}>
+          {used}$ /
+        </Text>
+        <Text weight={"bold"} variant={"text-base"}>
+          {amount}${" "}
+        </Text>
+      </div>
+      <Progress
+        value={(used / amount) * 100}
+        size="lg"
+        color="blue"
+        // color={{ backgroundColor: `${color}` }}
+      />
     </div>
->>>>>>> Stashed changes
- );
+  );
 };
 
 export default WalletCard;
-
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-
-// const Card = ({ icon, , add }) => {
-//   return (
-//     <Box
-//       className="flex justify-center items-center gap-20 flex-col h-80"
-//       color="gray">
-//       {add ? (
-//         <CategoryForm/>
-//       ) : (
-//         <>
-//           <Text variant="text-lg" weight="semibold">
-//             Transportation
-//           </Text>
-//           <div
-//             className="p-4 rounded-xl"
-//             style={{ backgroundColor: `${color}30` }}
-//           >
-//             {IconList.map((i) => {
-//               if (i.value === icon) {
-//                 return (
-//                   <FontAwesomeIcon icon={i.icon} size="2xl" color={color} />
-//                 );
-//               }
-//             })}
-//           </div>
-//           <Text weight="bold">300$</Text>
-//         </>
-//       )}
-//     </Box>
-//   );
-// };
-
-// export default Card;
--- src/components/Wallet/WalletForm.js
+++ src/components/Wallet/WalletForm.js
@@ -5,112 +5,133 @@ import FormInput from "../common/FormInput";
 import { Controller, useForm } from "react-hook-form";
 import Textarea from "../common/Textarea";
 import Icon from "../common/Icon";
+import ColorPicker from "../common/ColorPicker";
 
-const WalletForm =({children}) =>{
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+const WalletForm = ({ children }) => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-    const onSubmit = (d) => {
+  const onSubmit = (d) => {
     console.log(d);
-    };
+  };
 
-    return(
-        <>
-        <Button
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          onClick={() => document.getElementById("my_modal_1").showModal()}
-        >
+  return (
+    <>
+      <Button
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => document.getElementById("my_modal_1").showModal()}
+      >
+        <Text variant="text-sm" weight="bold">
+          Add Wallet
+        </Text>
+      </Button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box flex flex-col justify-center w-full">
+          <Text variant="text-xl" weight="semibold" className="text-center">
+            Create new wallet
+          </Text>
+          <div className="modal-action mx-0 block w-full">
+            <form method="dialog" className="flex flex-col gap-4">
+              <Button variant="close" className="text-black" size="fix">
+                x
+              </Button>
 
-          <Text variant="text-sm" weight="bold">Add Wallet</Text>
-        </Button>
-        <dialog id="my_modal_1" className="modal">
-          <div className="modal-box flex flex-col justify-center w-full">
-            <Text variant="text-xl" weight="semibold" className="text-center">
-              Create new wallet
-            </Text>
-            <div className="modal-action mx-0 block w-full">
-              <form method="dialog" className="flex flex-col gap-4">
-                <Button variant="close" className="text-black" size="fix">
-                  x
+              <Controller
+                name="wallet_name"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <div>
+                    <FormInput
+                      type="string"
+                      label="Name"
+                      name="wallet_name"
+                      value={field.value}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      labelType="side"
+                    />
+                  </div>
+                )}
+              />
+              <Controller
+                name="amount"
+                control={control}
+                defaultValue=""
+                rules={{ required: "Amount is required!" }}
+                render={({ field }) => (
+                  <div>
+                    <FormInput
+                      type="number"
+                      label="Amount"
+                      name="amount"
+                      value={field.value}
+                      onChange={(e) => field.onChange(e.target.value)}
+                      labelType="side"
+                    />
+                    {errors.amount && (
+                      <Text className="text-red-500 px-32 mt-3">
+                        {errors.amount.message}
+                      </Text>
+                    )}
+                  </div>
+                )}
+              />
+              <Controller
+                name="color"
+                control={control}
+                defaultValue="#f5d1e4"
+                render={({ field }) => (
+                  <div>
+                    <ColorPicker
+                      label="Color"
+                      value={field.value}
+                      onChange={(e) => field.onChange(e.target.value)}
+                    />
+                    {errors.type && (
+                      <Text className="text-red-500 px-32 mt-3">
+                        {errors.type.message}
+                      </Text>
+                    )}
+                  </div>
+                )}
+              />
+              <Controller
+                name="description"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <div>
+                    <Textarea
+                      label="Description"
+                      onChange={(e) => field.onChange(e.target.value)}
+                      value={field.value}
+                    />
+                  </div>
+                )}
+              />
+
+              <div className="flex justify-around">
+                <Button size="xl">Cancel</Button>
+                <Button
+                  size="xl"
+                  variant="roundOutline"
+                  onClick={handleSubmit(onSubmit)}
+                >
+                  Save
                 </Button>
-
-                <Controller
-                  name="wallet_name"
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <div>
-                      <FormInput
-                        type="string"
-                        label="Name"
-                        name="wallet_name"
-                        value={field.value}
-                        onChange={(e) => field.onChange(e.target.value)}
-                        labelType="side"
-                      />
-                    </div>
-                  )}
-                />
-                <Controller
-                  name="amount"
-                  control={control}
-                  defaultValue=""
-                  rules={{ required: "Amount is required!" }}
-                  render={({ field }) => (
-                    <div>
-                      <FormInput
-                        type="number"
-                        label="Amount"
-                        name="amount"
-                        value={field.value}
-                        onChange={(e) => field.onChange(e.target.value)}
-                        labelType="side"
-                      />
-                      {errors.amount && (
-                        <Text className="text-red-500 px-32 mt-3">
-                          {errors.amount.message}
-                        </Text>
-                      )}
-                    </div>
-                  )}
-                />
-                <Controller
-                  name="description"
-                  control={control}
-                  defaultValue=""
-                  render={({ field }) => (
-                    <div>
-                      <Textarea
-                        label="Description"
-                        onChange={(e) => field.onChange(e.target.value)}
-                        value={field.value}
-                      />
-                    </div>
-                  )}
-                />
-  
-                <div className="flex justify-around">
-                  <Button size="xl">Cancel</Button>
-                  <Button
-                    size="xl"
-                    variant="roundOutline"
-                    onClick={handleSubmit(onSubmit)}
-                  >
-                    Save
-                  </Button>
-                </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
-        </dialog>
-      </>
-    );
+        </div>
+      </dialog>
+    </>
+  );
 };
 
 export default WalletForm;
