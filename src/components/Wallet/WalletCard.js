a72dabd288f515e948d200a137a6db6a99350bb7
plqanhh <105305089+plqanhh@users.noreply.github.com>
2023-12-17 01:18:11 +0700

    Refactor Wallet page and components (generated by blackbox.ai )

--- src/components/Wallet/WalletCard.js
+++ src/components/Wallet/WalletCard.js
@@ -7,6 +7,7 @@ import { Progress } from "@material-tailwind/react";
 
 const WalletCard = ({ title, amount, used, color }) => {
  return (
+<<<<<<< Updated upstream
     <Box className="flex justify-center items-center flex-col gap-10 h-60 w-30" color={"gray"} >
         <>
             <Text  weight="semibold" variant="text-base" className="pt-10" >
@@ -26,6 +27,23 @@ const WalletCard = ({ title, amount, used, color }) => {
         
     </Box>
         
+=======
+    <div className='w-full max-w-sm p-4 bg-gray-100 border border-gray-200 rounded-lg shadow sm:p-8 space-y-10 hover:'>
+        <div>
+            <Text className="text-center ml-10 mr-10 " weight="semibold" variant="text-base" >{title}</Text>
+        </div>
+        <div>
+            <Icon svg={<WalletIcon/>} className="flex item-center" isHovered={"isHovered"} hoverColor={"#EF5DA8"} fillColor={"#FFFFFF"}/>
+        </div>
+        <div>
+            <Text weight={"bold"} variant={"text-base"}>{used}$ /</Text>
+            <Text weight={"bold"} variant={"text-base"}>{amount}$ </Text>
+        </div>
+        <div>
+            <Progress value={(used/amount)*100}/>
+        </div>
+    </div>
+>>>>>>> Stashed changes
  );
 };
 
