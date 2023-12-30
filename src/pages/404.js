ac1f814223b714ab44bf63ab9b5b24747a35af74
JuneDay <tthong.vox@gmail.com>
2023-12-30 21:39:20 +0700

    Redesign and rebuilding UI of 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Button from "../components/common/Button";
+import Image from "../assets/Error404.svg";
 const Error = () => {
   return (
-    <>
-      <section className="bg-white ">
-        <div className="py-8 px-4 mx-auto">
-          <div className="flex flex-col items-center justify-center h-[90vh]">
-            <p className="text-[#EF5DA8] text-7xl mb-4 font-bold">404</p>
-            <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl ">
-              Something's missing.
-            </p>
-            <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-              Sorry, we can't find that page.
-            </p>
-            <Button variant="cirFill" size="lg" href="#">
-              Back to Homepage
-            </Button>
-          </div>
-        </div>
-      </section>
-    </>
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="w-full flex flex-col justify-center items-center max-w-lg gap-6">
+        {/* 404 Image */}
+        <img src={Image} alt="404 Image" width={350} height={350} />
+        {/* Title */}
+        <span className="text-3xl font-bold">Thunder 404</span>
+        {/* Description */}
+        <p className="text-xl font-normal text-gray-600 text-justify">
+          Whoops! Looks like you've hit a digital dead end. The elusive 404 Not
+          Found page has claimed another victim. Fear not, brave adventurer!
+          Simply click on the button below to return to the safety of the
+          homepage and continue your digital quest. Happy trails!
+        </p>
+        <Button href="/">Back to Home</Button>
+      </div>
+    </div>
   );
 };
 
