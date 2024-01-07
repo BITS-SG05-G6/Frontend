2d7a444f930a40525d2c46eac130ea8ae8eeab2b
plqanhh <105305089+plqanhh@users.noreply.github.com>
2024-01-07 12:21:44 +0700

    Starting the Statis Page
    Start and fixing About Us page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
+import { AboutUs } from "./pages/AboutUs";
 import Transaction from "./pages/Transaction";
 import Bills from "./pages/Bills";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Category from "./pages/Category";
 import Wallet from "./pages/Wallet";
+import Error from "./pages/404";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />}></Route>
-      <Route path="/transaction" element={<Transaction />}></Route>
+      <Route path="/aboutus" element={<AboutUs/>}></Route>
+      {/* <Route path="/transaction" element={<Transaction />}></Route>
       <Route path="/transaction/:id" element={<Transaction />}></Route>
       <Route path="/invoices" element={<Bills />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/wallets" element={<Wallet />}></Route>
-
-      {/* <Route path="/login" element={<Login />}></Route> */}
       <Route path="/signup" element={<Signup />}></Route>
       <Route path="/category" element={<Category />}></Route>
+      <Route path="*" element={<Error />}></Route> */}
     </Routes>
   );
 }
