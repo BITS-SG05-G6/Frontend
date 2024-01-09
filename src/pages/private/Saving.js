6b09e423cbb823497c8be438d3c785ad0163cd43
xian-94 <s3957034@rmit.edu.vn>
2024-01-09 13:27:21 +0700

    hot fix overriding saving

--- src/pages/private/Saving.js
+++ src/pages/private/Saving.js
@@ -7,9 +7,19 @@ import { useEffect } from "react";
 import * as axiosInstance from '../../services/savingGoal'
 
 function Saving() {
-    useEffect(() => {
+    const {goals, handleUpdateGoal} = useContext(SavingContext);
 
-    })
+    // Delete a goal
+    async function handleDelete(id) {
+        try {
+            const res = await axiosInstance.deleteSavingGoal(id);
+            console.log(res);
+            handleUpdateGoal();
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
     return (
         <>
             <div className="flex justify-end px-6">
@@ -17,7 +27,7 @@ function Saving() {
             </div>
             <div className="px-10">
                 <div className="flex flex-col flex-1 gap-10 pr-5">
-                    <GoalList />
+                    <GoalList goals={goals} onDelete={handleDelete} />
                 </div>
             </div>
         </>
