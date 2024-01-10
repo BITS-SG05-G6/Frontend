61f1be38f56a373d299f4beb129528a835482bae
xian-94 <s3957034@rmit.edu.vn>
2024-01-10 16:32:35 +0700

    SCRUM-177 add chart for saving page

--- src/pages/private/SavingDetail.js
+++ src/pages/private/SavingDetail.js
@@ -4,6 +4,7 @@ import DetailPageLayout from "../../components/layout/DetailPageLayout";
 import GoalDetails from '../../components/SavingGoal/GoalDetails';
 import * as savingGoal from '../../services/savingGoal';
 import TransactionList from '../../components/Transaction/TransactionList';
+import MarkerChart from "../../components/Statistics/MarkerChart";
 
 function Details() {
     const { id } = useParams();
@@ -32,6 +33,7 @@ function Details() {
     return (
         <DetailPageLayout>
             <div className="flex flex-col flex-1 gap-10 pr-5">
+                <MarkerChart savingID={id}/>
                 <TransactionList transactions={goalTransactions} />
             </div>
             {/* render the components based on the data fetching */}
