2863ddc27a9b51c6153be28c0a02ad34d2e50e1d
vothanhthong <tthong.vox@gmail.com>
2024-01-15 10:10:59 +0700

    Make responsive for SideBar

--- src/components/layout/PrivatePageLayout.js
+++ src/components/layout/PrivatePageLayout.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "../common/Header";
 import SideBar from "../common/SideBar";
+import MobileSideBar from "../common/MobileSideBar";
 
 function PrivatePageLayout({ header, children, username }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -10,15 +11,18 @@ function PrivatePageLayout({ header, children, username }) {
   };
 
   return (
-    <>
-      <div className="hidden xl:block">
+    <div className="relative">
+      {/* <div className="absolute bottom-0 top-0 xl:hidden">
+        <MobileSideBar />
+      </div> */}
+      <div className="">
         <SideBar />
       </div>
-      <div className="flex flex-col gap-5 xl:pl-64">
+      <div className="flex flex-col gap-5 pl-16 xl:pl-64">
         <Header title={header} username={username} />
         {children}
       </div>
-    </>
+    </div>
   );
 }
 
